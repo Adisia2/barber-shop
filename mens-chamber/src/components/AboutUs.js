edc3c85528de4d6e8de5773f24ac7f9d3ceae56f
+John <+>
2023-01-25 16:01:13 +0300

    added a background color

--- mens-chamber/src/components/AboutUs.js
+++ mens-chamber/src/components/AboutUs.js
@@ -1,11 +1,21 @@
 import React from "react";
 
+
 const AboutUs = () => {
     return (
-        <div>
-        <h1>About Us</h1>
+        <div
+        style={{
+        padding: "100px",
+        background: "rgb(180, 180, 180)",
+        backgroundsize: "cover",
+        }}          
+        >
+        <div className="container ">
+        <h1 className="text-center ">About Us</h1>
         <p>Welcome to our barbershop! We are a locally owned and operated business that has been serving the community for over 10 years. Our team of experienced barbers specialize in a variety of cuts and styles for men, women, and children. We pride ourselves on providing excellent customer service and a comfortable, relaxed atmosphere for our clients.</p>
       <p>We also offer a wide range of grooming products to help you maintain your look at home. From hair care to skin care, we have everything you need to look and feel your best.</p>
+        </div>
+
         </div>
     );
     }
