3d723d296d524dec066bff54e2655d6d7ccc117f
+John <+>
2023-01-27 00:23:22 +0300

    customized how to choose a style

--- mens-chamber/src/components/Services.js
+++ mens-chamber/src/components/Services.js
@@ -1,38 +1,67 @@
 import React,{useEffect, useState} from "react";
 
 const Services = () => {
-    const [services, setServices] = useState([]);
+const [services, setServices] = useState([]);
+const [selectedService, setSelectedService] = useState(JSON.parse(localStorage.getItem('selectedService') || '[]'))
 
-    useEffect(() => {
-        fetch('http://localhost:5000/services')
-            .then(res => res.json())
-            .then(data => {
-                setServices(data);
-                console.log(data);
-            })
+
+useEffect(() => {
+    fetch('http://localhost:5000/services')
+        .then(res => res.json())
+        .then(data => {
+            setServices(data);
+            console.log(data);
+        })
+}, []);
+
+const chooseStyle = service => {
+    if (!selectedService.find(s => s.id === service.id)) {
+        setSelectedService([...selectedService, service]);
     }
-    , [])
+}
+const removeStyle = service => {    
+    setSelectedService(selectedService.filter(s => s.id !== service.id));
+}
 
-    return (
-        <div className="container mt-5">
-        <div className="row p-4">
-            {services.map(service => {
-                return (
-                    <div key={service.id} className="card m-3 text-center" style={{width:"18rem"}}>
-                        <img src={service.image} className="card-img-top" alt={service.name}/>
-                        <div className="card-body">
-                            <h5 className="card-title">{service.name}</h5>
-                            <p className="card-text">{service.description}</p>
-                            
-                            <button type="button" className="btn btn-primary">Book</button>
-                        </div>
+return (
+    
+    <div className="container mt-5">
+    <div className="col-10 p-4 bg-secondary mx-auto">
+        <div className="row">                                                                                                                                                                                                                                  
+        {selectedService.map(service => {
+            return (
+                <div key={service.id} className="card m-3" style={{width:"18rem"}}>
+                    <img src={service.image} className="card-img-top" alt={service.name}/>
+                    <div className="card-body">
+                        <h5 className="card-title">{service.name}</h5>
+                        <p className="card-text">{service.description}</p>
+                        <button onClick={() => removeStyle(service)} type="button" className="btn btn-primary">Remove</button>
                     </div>
-                )
-            }
-            )}
-        </div>
-        </div>
-    );
+                </div>
+            )   
+        }
+        )}
+    </div>
+    </div>
+
+    <div className="row p-4 container">
+        {services.map(service => {
+            return (
+                <div  key={service.id} className="card m text-center" style={{width:"18rem"}}>
+                    <img src={service.image} className="card-img-top" alt={service.name}/>
+                    <div className="card-body">
+                        <h5 className="card-title">{service.name}</h5>
+                        <p className="card-text">{service.description}</p>
+                        <button onClick={() => chooseStyle(service)} type="button" className="btn btn-primary">Book</button>
+                    </div>
+                </div>
+            )
+        }
+        )}
+    </div>
+    </div>
+    
+);
 };
 
 export default Services;
