782fe533671ecb64a711620f080780a4347bfcbb
+John <+>
2023-03-12 20:18:07 +0300

    updated alert on form submission

--- mens-chamber/src/components/Appointment.js
+++ mens-chamber/src/components/Appointment.js
@@ -1,22 +1,15 @@
 import React, { useState,  } from 'react';
-
-import 'react-datepicker/dist/react-datepicker.css';
-
+import Swal from 'sweetalert2';
 
 const Appointments = () => {
-    
     
     const [name, setName] = useState("");
     const [phone, setPhone] = useState("");
     const [email, setEmail] = useState("");
     
-
- 
-
     function handleNameChange(event) {
         setName(event.target.value);
     }
-
     function handlePhoneChange(event) {
         setPhone(event.target.value);
     }
@@ -25,45 +18,42 @@ const Appointments = () => {
         setEmail(event.target.value);
     }
 
-  
-
     function handleSubmit(event) {
         event.preventDefault();
-       
+      
         const formData = {
-            name: name,
-            phone: phone,
-            email: email,
-            // service: service,
-            // date: date,
-         
+          name: name,
+          phone: phone,
+          email: email,
         };
+      
         console.log(formData);
-        // Send the form data
-        setName("");
-        setPhone("");
-        setEmail("");
-        // setService("");
-        // setDate("");
-
-        // Send the form data to your server or handle it
-        // locally
-        fetch("https://kinyozi.up.railway.app/appointments/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData),
-      })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-            }
-            )
-           
-    }
+      
+        fetch('https://kinyozi.up.railway.app/appointments/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setName('');
+            setPhone('');
+            setEmail('');
+      
+            // Show success message
+            Swal.fire({
+              title: 'Success!',
+              text: 'Your appointment has been booked.',
+              icon: 'success',
+              confirmButtonText: 'OK',
+            });
+          });
+      }
+      
     
-
     return (
         <div className="container mt-5">
             <div className="row justify-content-center">
@@ -86,22 +76,7 @@ const Appointments = () => {
                                     <label htmlFor="email">Email address</label>
                                     <input type="email" className="form-control" id="email" name="email" value={email} onChange={handleEmailChange} />
                                 </div>
-                                {/* <div className="form-group">
-                                    <label htmlFor="service">Service</label>
-                                    <select className="form-control" id="service" name="service" value={service} onChange={handleServiceChange}>
-                                        <option value="">Select a service</option>
-                                        <option value="Haircut">Haircut</option>
-                                        <option value="Hair coloring">Hair coloring</option>
-                                        <option value="Traditional warm towel">Traditional warm towel</option>
-                                        <option value="Beard Trimming">Beard Trimming</option>
-                                        <option value="Facial Grooming">Facial Grooming</option>
-                                        <option value="Hair Styling">Hair Styling</option>
-                                    </select>
-                                </div> */}
-                                {/* <div className="form-group">
-                                    <label htmlFor="date">Date</label>
-                                    <input type="text" className="form-control" id="date" name="date" value={date} onChange={handleDateTimeChange} />
-                                </div> */}
+                               
                                 <button type="submit" className="btn btn-primary mt-3">Book Appointment</button>
                             </form>
                         </div>
